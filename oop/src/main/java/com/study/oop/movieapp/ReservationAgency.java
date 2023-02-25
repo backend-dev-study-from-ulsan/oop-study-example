d82d08f5f53e6fb5fda00fbef7197acbb21c7143
jaeseok-go <gojeasuk2@naver.com>
2023-02-25 16:03:03 +0900

    feat: 총 요금계산하는 책임 Screening으로 이동

--- oop/src/main/java/com/study/oop/movieapp/ReservationAgency.java
+++ oop/src/main/java/com/study/oop/movieapp/ReservationAgency.java
@@ -1,34 +1,7 @@
 package com.study.oop.movieapp;
-
-import com.study.oop.movieapp.condition.DiscountCondition;
-import com.study.oop.movieapp.condition.DiscountConditionType;
-
-import java.time.DayOfWeek;
-import java.time.LocalTime;
-
 public class ReservationAgency {
     public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
-        Movie movie = screening.getMovie();
-
-        boolean discountable = movie.isDiscountable(screening.getWhenScreened(), screening.getSequence());
-
-        Money fee = Money.ZERO;
-        if (discountable) {
-            switch (movie.getMovieType()) {
-                case AMOUNT_DISCOUNT:
-                    fee = movie.calculateAmountDiscountedFee();
-                    break;
-                case PERCENT_DISCOUNT:
-                    fee = movie.calculatePercentDiscountedFee();
-                    break;
-                case NONE_DISCOUNT:
-                    fee = movie.calculateNoneDiscountedFee();
-                    break;
-            }
-        } else {
-            fee = movie.getFee();
-        }
-
+        Money fee = screening.calculateFee(audienceCount);
         return new Reservation(customer, screening, fee, audienceCount);
     }
 }
--- oop/src/main/java/com/study/oop/movieapp/Screening.java
+++ oop/src/main/java/com/study/oop/movieapp/Screening.java
@@ -6,10 +6,28 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }
