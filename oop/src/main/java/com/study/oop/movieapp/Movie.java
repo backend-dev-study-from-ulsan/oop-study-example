678c23e51a3b85386878f7ede64cbaba681fb034
sejong <sejong055@gmail.com>
2023-03-05 16:28:32 +0900

    refact: Movie에서 calculateMovieFee 책임
    정보 전문가에게 책임을 할당함

--- oop/src/main/java/com/study/oop/movieapp/Movie.java
+++ oop/src/main/java/com/study/oop/movieapp/Movie.java
@@ -16,47 +16,45 @@ public class Movie {
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
+
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
 
-    public Money calculateAmountDiscountedFee() {
-        if (movieType != MovieType.AMOUNT_DISCOUNT) {
-            throw new IllegalArgumentException();
+    public Money calculateMovieFee(Screening screening) {
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
         }
-
-        return fee.minus(discountAmount);
-    }
-
-    public Money calculatePercentDiscountedFee() {
-        if (movieType != MovieType.PERCENT_DISCOUNT) {
-            throw new IllegalArgumentException();
-        }
-
-        return fee.minus(fee.times(discountPercent));
-    }
-
-    public Money calculateNoneDiscountedFee() {
-        if (movieType != MovieType.NONE_DISCOUNT) {
-            throw new IllegalArgumentException();
-        }
-
         return fee;
     }
 
-    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
-        for (DiscountCondition condition : discountConditions) {
-            if (condition.getType() == DiscountConditionType.PERIOD) {
-                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
-                    return true;
-                }
-            } else {
-              if (condition.isDiscountable(sequence)) {
-                  return true;
-              }
-            }
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream()
+            .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private Money calculateDiscountAmount() {
+        switch (movieType) {
+            case AMOUNT_DISCOUNT:
+                return calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT:
+                return calculatePercentDiscountAmount();
+            case NONE_DISCOUNT:
+                return calculateNoneDiscountAmount();
         }
 
-        return false;
+        throw new IllegalStateException();
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    private Money calculatePercentDiscountAmount(){
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
     }
 }
